36e17e5707b5e7eebac133293823e6aa54e0f654
airqz <XXX>
2019-10-15 13:22:02 +0200

    add gh-p design

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $('#search').keyup(function() {
-	var searchField = $('#search').val();
-	var myExp = new RegExp(searchField, "i");
+	let searchField = $('#search').val(),
+		myExp = new RegExp(searchField, "i");
 	$.getJSON('data.json', function(data) {
-		var output = '<ul class="searchresults">';
+		let output = '<ul class="searchresults">';
 		$.each(data, function(key, val) {
 			if ((val.name.search(myExp) != -1) ||
 			(val.bio.search(myExp) != -1)) {
 				output += '<li>';
-				output += '<h1>'+ val.shortname +'</h1>';	
-			    output += '<a href="'+ val.github +'" target="_blank"> Open <i class="fa fa-github"></i> Github</a>';	
-				output += '<p>'+ val.bio +'</p>';	
-				output += '<h2>'+ val.tech + '</h2>';
-				output += '<strong>'+ val.name + '</strong>';
-				output += '<img width="52px" src="'+ val.image +'" />';
-			    output += '<a href="'+ val.website +'" target="_blank"> Open <i class="fa fa-home"></i> Website</a>';
+					output += '<h1>'+ val.shortname +'</h1>';
+			    		output += '<a href="'+ val.github +'" target="_blank"> Open <i class="fa fa-github"></i> Github</a>';
+						output += '<p>'+ val.bio +'</p>';
+						output += '<h2>'+ val.tech + '</h2>';
+						output += '<strong>'+ val.name + '</strong>';
+						output += '<img width="52px" src="'+ val.image +'" />';
+			    	output += '<a href="'+ val.website +'" target="_blank"> Open <i class="fa fa-home"></i> Website</a>';
 				output += '</li>';
 			}
 		});
